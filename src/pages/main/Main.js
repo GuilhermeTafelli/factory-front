6e13ff85533847cc9079f26ac3bf7ad843eeb122
Guilherme Tafelli <38620644+GuilhermeTafelli@users.noreply.github.com>
2024-12-17 14:52:47 -0300

    Update Main.js

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -15,7 +15,7 @@ const Main = () => {
     }, [])
 
     const loadPurchaseOrders = async () => {
-        const response = await api.get('/purchase-orders?page=1&size=200');
+        const response = await api.get('/purchase-orders?page=1&size=20');
         setState({
             purchaseOrders: response.data.purchaseOrders,
             page: response.data.page,
