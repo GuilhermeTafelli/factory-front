8634979b1df3faceafa3cefb39ed5e700a7c1efc
Guilherme Rodrigues Tafelli da Silva <guilherme.tafelli@MacBook-Pro.local>
2025-01-15 16:56:53 -0300

    deploy

---
+++ build/precache-manifest.5c1e8b31809c9c2c95e8b209becf984c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d205f6dd19e54292cda78fa820aa2860",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad69796c736ab0c781c1",
+    "url": "/static/js/2.b86089e9.chunk.js"
+  },
+  {
+    "revision": "7d49f9d910c4ca2b09c4f1b0d1210bb5",
+    "url": "/static/js/2.b86089e9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d66dc193078b10657450",
+    "url": "/static/js/main.6b90edb8.chunk.js"
+  },
+  {
+    "revision": "b64aa6aa05dc0a0491d4",
+    "url": "/static/js/runtime-main.972d34d8.js"
+  }
+]);
